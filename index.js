3942f8e8206ac01b5e8b49aae63d02a1a6763f9a
knochenmark <marktodd.hl@gmail.com>
2018-03-03 15:28:39 +0100

    ✨ add first iteration of the leaderboard

--- index.js
+++ index.js
@@ -1,13 +1,114 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class HelloMessage extends React.Component {
+function xhrRequest(scope){
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://fcctop100.herokuapp.com/api/fccusers/top/'+ scope.state.column, true);
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      scope.setState({[scope.state.column]: JSON.parse(this.response)});
+    } else {
+      console.error(this.status, this.statusText);
+    }
+  };
+
+  request.onerror = function() {
+    console.error(this.status, this.statusText);
+  };
+
+  request.send();
+}
+
+class Leaderboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      recent: [],
+      alltime: [],
+      column: "recent"
+    }
+  }
+  getData() {
+    if(this.state[this.state.column].length === 0){
+      xhrRequest(this);
+    }
+  }
+  componentDidMount() {
+    this.getData();
+  }
+
+
   render() {
-    return <div> Hello {
-      this.props.name
-    } </div>;
+    let users = this.state[this.state.column];
+    let footerText = "Made by ";
+    return <div className='leaderboard'>
+      <div className='header'>
+        <h1>{this.props.title}</h1>
+      </div>
+      <CamperTable users = {users} />
+      <div className='made-by'>
+        {footerText}
+        <a href='https://github.com/Knochenmark'>Knochenmark</a>
+      </div>
+    </div>
+  }
+}
+
+class CamperTable extends React.Component {
+  render() {
+    let camperList = this.props.users.map((user,index)=>{
+      return (
+        <CamperRow rank={index+1} user={user} key={user.username}/>
+      );
+    });
+
+    let selected = this.props.column;
+
+    return <div className='camper-table'>
+    <div className='camper-table-header'>
+      <div className='camper-table-rank'>
+        Rank
+      </div>
+      <div className='camper-table-camper'>
+        Camper
+      </div>
+      <div className='camper-table-last-30'>
+        Points of last 30 days
+      </div>
+      <div className='camper-table-all-time'>
+        All time points
+      </div>
+  </div>
+  <div className='camper-table-users'>
+    {camperList}
+  </div>
+  </div>;
+  }
+}
+
+class CamperRow extends React.Component {
+  render() {
+    let link = 'https://www.freecodecamp.org' + this.props.user.username;
+    return <div className='camper-row-user'>
+    <div className='camper-row-rank'>
+      {this.props.rank}
+    </div>
+    <div className='camper-row-camper'>
+      <a href={link}>
+        <img src= {this.props.user.img}/>
+        {this.props.user.username}
+      </a>
+    </div>
+    <div className='camper-row-last-30'>
+      {this.props.user.recent}
+    </div>
+    <div className='camper-row-all-time'>
+      {this.props.user.alltime}
+    </div>
+  </div>;
   }
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render( < HelloMessage name = "Jane" / > , mountNode);
+ReactDOM.render( < Leaderboard title = "FCC Camper Leaderboard" / > , mountNode);
